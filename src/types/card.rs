40676af9d642ea78d48e876c91771c686e19138c
septante <130835353+septante@users.noreply.github.com>
2023-04-29 12:31:00 +0200

    impl useful traits for Card

--- src/types/card.rs
+++ src/types/card.rs
@@ -1,3 +1,9 @@
+use std::{
+    cmp::Ordering,
+    fmt::{self, Formatter},
+    hash::{Hash, Hasher},
+};
+
 use dyn_clonable::clonable;
 
 #[clonable]
@@ -8,6 +14,44 @@ pub trait Card: Clone + Send + Sync {
         ""
     }
     fn cost(&self) -> Cost;
+}
+
+impl fmt::Display for dyn Card {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.name())
+    }
+}
+
+impl fmt::Debug for dyn Card {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.name())
+    }
+}
+
+impl Hash for dyn Card {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.name().to_lowercase().hash(state);
+    }
+}
+
+impl PartialEq for dyn Card {
+    fn eq(&self, other: &Self) -> bool {
+        self.name().to_lowercase().eq(&other.name().to_lowercase())
+    }
+}
+
+impl Eq for dyn Card {}
+
+impl Ord for dyn Card {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.name().to_lowercase().cmp(&other.name().to_lowercase())
+    }
+}
+
+impl PartialOrd for dyn Card {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
 }
 
 #[non_exhaustive]
