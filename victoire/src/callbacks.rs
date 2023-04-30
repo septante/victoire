f2cc8cea42ef49b0b90e17e8527793cf7fecf148
septante <130835353+septante@users.noreply.github.com>
2023-04-30 02:23:50 +0200

    impl AttackTarget::PlayerOfChoice

--- victoire/src/callbacks.rs
+++ victoire/src/callbacks.rs
@@ -8,7 +8,7 @@ use dyn_clonable::clonable;
 /// Trait for getting input from players while card effects are occuring
 pub trait Callbacks: Clone + Send + Sync {
     /// Prompt the given player for a card from the supply
-    fn choose_card_from_supply(&self, player_number: usize) -> Option<Box<dyn Card>>;
+    fn choose_card_from_supply(&self, player_index: usize) -> Option<Box<dyn Card>>;
     /// Prompt the player for one or more cards from their hand
     fn choose_cards_from_hand(&self, count: usize, message: &str) -> Vec<usize>;
     /// Prompt the player for one or more cards from their discard
@@ -29,5 +29,7 @@ pub trait Callbacks: Clone + Send + Sync {
     /// Optionally prompt the player for one or more cards cards from the trash
     fn choose_cards_from_trash_opt(&self, count: usize, message: &str) -> Option<Vec<usize>>;
     /// Prompt the given player with a yes/no question
-    fn get_player_consent(&self, player_number: usize, prompt: &str) -> bool;
+    fn get_player_consent(&self, player_index: usize, prompt: &str) -> bool;
+    /// Prompt player for one or more player indices
+    fn choose_players(&self, player_index: usize, count: usize, prompt: &str) -> Vec<usize>;
 }
